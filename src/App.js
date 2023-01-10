287b37b9c7ccf1c5b06516e5fa228eb120e744eb
Filipe Garrote <filipe.garrote@gmail.com>
2023-01-10 00:31:32 -0300

    feat: realizada a estruturação dos arquivos com a definição das rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Checkout from "./pages/Checkout";
+import MainPage from "./pages/Mainpage";
+import SignIn from "./pages/Sign-in";
+import SignUp from "./pages/Sign-up";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
