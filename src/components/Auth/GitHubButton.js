9f9676f24f4546e0a95dd7cf5e43f016f1f3cb1e
Filipe Garrote <filipe.garrote@gmail.com>
2023-01-17 17:04:09 -0300

    feat: adicionado login social com GitHub

--- src/components/Auth/GitHubButton.js
+++ src/components/Auth/GitHubButton.js
@@ -1,9 +1,37 @@
 import styled from "styled-components";
 import { BsGithub } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { signInWithPopup, GithubAuthProvider } from "firebase/auth";
+import { auth } from "../../services/firebase";
+import { postSocialLogin } from "../../services/axios";
 
 export default function GitHubButton() {
+	const navigate = useNavigate()
+
+  async function handleGitHubLogin(){
+    const provider = new GithubAuthProvider();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+			console.log(result);
+      const user = {
+        name: result.user.reloadUserInfo.screenName,
+        accessToken: result.user.uid,
+        email: result.user.email ? result.user.email : `${result.user.reloadUserInfo.screenName}@github.com`
+      }
+
+      const token = await postSocialLogin(user);
+      localStorage.setItem("token", token)
+      localStorage.setItem("name", user.name)
+      navigate("/")
+    } catch (error) {
+			console.log(error)
+      alert("Deu ruim, meu bom")
+    }
+  }
+
 	return (
-		<StyledGitHubButton>
+		<StyledGitHubButton onClick={handleGitHubLogin}>
 			<BsGithub />
 			<span>Entrar com GitHub</span>
 		</StyledGitHubButton>
